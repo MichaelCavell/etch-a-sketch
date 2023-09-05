499a60f7b3a5d96a81d131ddbe8234cea4d61656
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2023-09-05 15:29:58 -0400

    Set up hover effect
    Divs change color when mouse passes over leaving a pixelated trail like a pen would

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const container = document.getElementById('container');
         squareDiv.classList.add('squareDiv');
         container.appendChild(squareDiv);
     }
+
+
+const squareDivs = document.querySelectorAll(".squareDiv");
+squareDivs.forEach(function(square) {
+    square.addEventListener("mouseover", function() {
+        square.classList.remove("squareDiv");
+        square.classList.add("mouseoverDiv");
+    });
+})
