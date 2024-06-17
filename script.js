f2f8e5add63e63cc17cc46e3f4ae76b24a4c8c22
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2024-06-17 14:59:20 -0400

    Set up a “hover” effect for the pixel divs
    Set up a “hover” effect so that the grid divs
    change color when your mouse passes over them,
    leaving a (pixelated) trail through your grid like
    a pen would.

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 const container = document.querySelector('.pixel-container');
+let pixels = document.querySelectorAll('.pixel');
+let activePixels = document.querySelectorAll('.active-pixel');
+
+function addPixelListeners() {
+    pixels.forEach(function(pixel, index) {
+        pixel.addEventListener('mouseover', function() {
+            makeActive(pixels[index]);
+        })
+    })
+}
+
+function sketchRefresh() {
+    pixels.forEach(function(pixel) {
+        pixel.classList.remove('active-pixel');
+        pixel.classList.remove('drawn-pixel');
+    })
+}
+
+function makeInactive(pixel) {
+    pixel.classList.remove('active-pixel');
+    pixel.classList.add('drawn-pixel');
+}
+
+function makeActive(pixel) {
+    pixel.classList.add('active-pixel');
+    activePixels = document.querySelectorAll('.active-pixel');
+    activePixels.forEach(function(activePixel, index) {
+        activePixel.addEventListener('mouseout', function() {
+            makeInactive(activePixels[index]);
+        })
+    })
+}
 
 function makeGrid(sideCount) {
     for (let i = 0; i < sideCount; i++) {
@@ -11,6 +43,10 @@ function makeGrid(sideCount) {
             column.appendChild(pixel); 
         }
     }
+    pixels = document.querySelectorAll('.pixel');
+    activePixels = document.querySelectorAll('.active-pixel');
+    addPixelListeners();
+    sketchRefresh();
 }
 
-makeGrid(16);
+makeGrid(100);
